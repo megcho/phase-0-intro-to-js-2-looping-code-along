1b594bcf42bbac153b1216c07fd2c75224cb756d
megan cho <meganlisacho@gmail.com>
2021-03-25 20:35:56 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const name = ["Ada", "Brendan", "Ali"];
+
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        console.log(`Thank you ${name[i]} for the wonderful birthday gift!`);
+    }
+    return name;
+}
+
+writeCards(name);
+
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+let countDown = 20;
+while (countDown >= 0) {
+  console.log(countDown--);
+} 
